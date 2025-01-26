421b1f7b7ef23e7768e34c6daed5885e9a5bfcf3
dyussembaikyzya <diusembaikyzy@mail.ru>
2025-01-26 21:05:23 +0500

    home_commentsy

---
+++ lab1/home_comments.py
@@ -0,0 +1,43 @@
+# Home - into
+print("Hello world")
+# Hello world
+
+#Get Started
+import sys
+print(sys.version)
+# 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)]
+
+# Syntax correct:
+if 5 > 2:
+    print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+# Syntax Error:
+"""
+if 5 > 2:
+print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!")
+        print("Five is greater than two!")
+"""
+#   File "c:\Users\user\Desktop\pp2\labs\lab1\home_comments.py", line 21 
+# print("Five is greater than two!") ^^^^^
+# IndentationError: expected an indented block after 'if' statement on line 20
+
+# Comments
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
