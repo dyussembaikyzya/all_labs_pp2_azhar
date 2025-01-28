1893b4439c42dc821f41a99a061fc5e8943751ce
dyussembaikyzya <diusembaikyzy@mail.ru>
2025-01-28 11:42:50 +0500

    lab3

--- lab3/classes/1.py
+++
@@ -1,9 +0,0 @@
-class String:
-    def getstring(self):
-        self.sentence=input("Sentense:")
-    def printstring(self):
-        print("With upper case:"+self.sentence.upper())
-        
-mystring=String()
-mystring.getstring()
-mystring.printstring()
